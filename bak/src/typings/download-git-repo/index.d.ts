2a2c3f5cc442f90e55698224a75baaf3f2591f9e
mzli11 <mzli11@iflytek.com>
2021-04-29 20:52:15 +0800

    v1

--- bak/src/typings/download-git-repo/index.d.ts
+++
@@ -1,8 +0,0 @@
-import ErrnoException = NodeJS.ErrnoException;
-
-function download(source: string, dest: string, opts: { clone: boolean; }, callback: (err: ErrnoException | null) => void): void;
-function download(source: string, dest: string, callback: (err: ErrnoException | null) => void): void;
-
-declare module 'download-git-repo' {
-  export default download;
-}
